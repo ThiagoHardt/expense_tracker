46122b4661cfce0df27c163a0a727ba3158d6cd3
thardtsalessense <thiago.hardts@gmail.com>
2021-08-04 16:17:36 +0100

    Added conditionals to form

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import ExpenseItem from "./ExpenseItem";
+import ExpensesList from "./ExpensesList";
 import ExpenseFilter from "./ExpenseFilter";
 import Card from "../UI/Card";
 import "./Expenses.css";
@@ -21,14 +21,7 @@ const Expenses = (props) => {
         onChangeFilter={filterChangeHandler}
         selected={filteredYear}
       />
-      {filteredExpenses.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
+      <ExpensesList items={filteredExpenses} />
     </Card>
   );
 };
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,18 +1,38 @@
-import "./NewExpense.css";
+import { useState } from "react";
 import NewExpenseForm from "./NewExpenseForm";
+import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+
   const onSaveExpenseData = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setIsEditing(false);
+  };
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
   };
 
   return (
     <div className="new-expense">
-      <NewExpenseForm onSaveExpenseData={onSaveExpenseData} />
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )}
+      {isEditing && (
+        <NewExpenseForm
+          onSaveExpenseData={onSaveExpenseData}
+          onCancel={stopEditingHandler}
+        />
+      )}
     </div>
   );
 };
